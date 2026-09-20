6e89bca24881f3c2d9f13bdaf1304b53455ab344
spook_lol <elitc10@icloud.com>
2026-09-20 13:45:39 +1000

    Save velocity change trigger mode

--- src/system/config.c
+++ src/system/config.c
@@ -220,6 +220,10 @@ static void load_prediction_config(toml_datum_t table, prediction_settings_t *se
       if (config_int(table, key, &integer) && integer >= PREDICTION_COMPARE_EQUAL &&
           integer <= PREDICTION_COMPARE_CHANGED)
         rule->comparison = (prediction_rule_comparison_t)integer;
+	  prediction_config_key(key, sizeof(key), line_index, rule_index, "change_mode");
+	  if (config_integer(table, key, &integer) &&
+		  integer >= PREDICTION_CHANGE_ANY && integer <= PREDICTION_CHANGE_DECREASING)
+	    rule->change_mode = (prediction_change_mode_t)integer;
     }
   }
 }
@@ -277,6 +281,9 @@ static void write_prediction_config(FILE *fp, const prediction_settings_t *setti
       prediction_config_key(key, sizeof(key), line_index, rule_index, "comparison");
       write_toml_key(fp, key);
       fprintf(fp, " = %d\n", (int)rule->comparison);
+	  prediction_config_key(key, sizeof(key), line_index, rule_index, "change_mode");
+	  fprintf(fp, "%s", key);
+	  fprintf(fp, " = %d\n", (int)rule->change_mode);
     }
   }
 }
